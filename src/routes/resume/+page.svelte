df835e5511e750ed621093de459c278f653e5da0
Arshveer Gahir <arshveersinghgahir@gmail.com>
2025-08-12 21:41:43 -0400

    update resume

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	let resumePdfUrl =
-		'https://drive.google.com/file/d/1vp-GXJM62c1plobpRwqsrlGDWBnQoes4/view?usp=sharing';
-	let googleViewerUrl = `https://drive.google.com/file/d/1vp-GXJM62c1plobpRwqsrlGDWBnQoes4/preview`;
+		'https://drive.google.com/file/d/1Md-o3o8KjDOYRMB28jDNdPgOgAyrXUMo/view?usp=sharing';
+	let googleViewerUrl = `https://drive.google.com/file/d/1Md-o3o8KjDOYRMB28jDNdPgOgAyrXUMo/preview`;
 </script>
 
 <svelte:head>
